1dfaf9177eac38eb6227c54fe8f8a1a0e1f405ea
Mehmet-D02 <71323491+Mehmet-D02@users.noreply.github.com>
2023-12-24 00:40:39 +0300

    Update server.js

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const mysql = require('mysql2');
 
 // create the connection to database
 const connection = mysql.createConnection({
-  host: 'db',
+  host: 'project_db_1',
   port: '3306',
   user: 'root',
   database: 'test',
